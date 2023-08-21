49c8fa206ef974ef24a7af71be1e8a68d762017a
Oshioke-Salaki <salaki1902@gmail.com>
2023-08-21 16:31:48 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Weather from './Weather';
 
 function convertToFlag(countryCode: string) {
@@ -10,7 +10,7 @@ function convertToFlag(countryCode: string) {
 }
 
 const App: React.FC = () => {
-  const [location, setLocation] = useState<string>('Lisbon');
+  const [location, setLocation] = useState<string>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [displayLocation, setDisplayLocation] = useState<string>('');
   const [weather, setWeather] = useState<{
@@ -20,35 +20,41 @@ const App: React.FC = () => {
     weathercode: number[];
   }>();
 
-  const fetchWeather = async () => {
-    try {
-      setIsLoading(true);
-      // 1) Getting location (geocoding)
-      const geoRes = await fetch(
-        `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-      );
-      const geoData = await geoRes.json();
-      console.log(geoData);
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        setIsLoading(true);
+        // 1) Getting location (geocoding)
+        const geoRes = await fetch(
+          `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+        );
+        const geoData = await geoRes.json();
+        console.log(geoData);
 
-      if (!geoData.results) throw new Error('Location not found');
+        if (!geoData.results) throw new Error('Location not found');
 
-      const { latitude, longitude, timezone, name, country_code } =
-        geoData.results.at(0);
-      setDisplayLocation(`${name} ${convertToFlag(country_code)}`);
+        const { latitude, longitude, timezone, name, country_code } =
+          geoData.results.at(0);
+        setDisplayLocation(`${name} ${convertToFlag(country_code)}`);
 
-      // 2) Getting actual weather
-      const weatherRes = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
-      );
-      const weatherData = await weatherRes.json();
-      setWeather(weatherData.daily);
-      console.log(weatherData.daily);
-    } catch (err) {
-      console.error(err);
-    } finally {
-      setIsLoading(false);
+        // 2) Getting actual weather
+        const weatherRes = await fetch(
+          `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+        );
+        const weatherData = await weatherRes.json();
+        setWeather(weatherData.daily);
+        console.log(weatherData.daily);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
     }
-  };
+
+    if (!location) return;
+
+    fetchWeather();
+  }, [location]);
 
   return (
     <div className="app">
@@ -61,9 +67,8 @@ const App: React.FC = () => {
           onChange={(e) => setLocation(e.target.value)}
         />
       </div>
-      <button onClick={fetchWeather}>Get weather</button>
       {isLoading && <p className="loader">Loading...</p>}
-      {weather?.weathercode && (
+      {weather?.weathercode && !isLoading && (
         <Weather weather={weather} location={displayLocation} />
       )}
     </div>
